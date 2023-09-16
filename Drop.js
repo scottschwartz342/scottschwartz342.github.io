25d318441470231965324d8b0445b51f22c50d6d
scottschwartz342 <scottschwartz97@gmail.com>
2023-09-16 13:01:53 -0400

    project drop down stuff

---
+++ Drop.js
@@ -0,0 +1,27 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+var inUse = false;
+function dropAction(n) {
+     if(inUse==false)
+     {
+          var result = "dd" + n;
+          document.getElementById(result).classList.toggle("show");
+          inUse=true;
+     }
+}
+     
+     // Close the dropdown if the user clicks outside of it
+     window.onclick = function(event) {
+     if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+               openDropdown.classList.remove('show');
+          }
+          }
+          inUse=false;
+     }
+}
