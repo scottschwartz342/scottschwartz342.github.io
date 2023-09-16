0726b82f7c81840245b19853c6faedd467e52a21
scottschwartz342 <scottschwartz97@gmail.com>
2023-09-16 13:12:11 -0400

    fix touch

--- Drop.js
+++ Drop.js
@@ -9,33 +9,31 @@ function dropAction(n) {
           document.getElementById(result).classList.toggle("show");
           inUse=true;
      }
+     else if(inUse==true)
+     {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+               openDropdown.classList.remove('show');
+          }
+          }
+          inUse=false;
+     }
 }
      
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-     if (!event.target.matches('.dropbtn')) {
-          var dropdowns = document.getElementsByClassName("dropdown-content");
-          var i;
-          for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-               openDropdown.classList.remove('show');
-          }
-          }
-          inUse=false;
-     }
-}
-
-window.ontouchstart = function(event) {
-     if (!event.target.matches('.dropbtn')) {
-          var dropdowns = document.getElementsByClassName("dropdown-content");
-          var i;
-          for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-               openDropdown.classList.remove('show');
-          }
-          }
-          inUse=false;
-     }
-}
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//      if (!event.target.matches('.dropbtn')) {
+//           var dropdowns = document.getElementsByClassName("dropdown-content");
+//           var i;
+//           for (i = 0; i < dropdowns.length; i++) {
+//           var openDropdown = dropdowns[i];
+//           if (openDropdown.classList.contains('show')) {
+//                openDropdown.classList.remove('show');
+//           }
+//           }
+//           inUse=false;
+//      }
+// }
