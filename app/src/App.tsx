85d7500c5891c65bc21f88fc74f2f931a482a542
scottschwartz342 <scottschwartz342@gmail.com>
2026-07-29 13:27:31 -0400

    dog background

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,30 +1,13 @@
 import './App.css'
-import horse from './assets/horse.jpg'
 
-import { Col, Image, Row } from 'antd';
 import CustomCarousel from './components/CutomCarousel';
-import ShaderBackground from './components/ShaderBackground';
 import './components/components.css';
 
 const App = () => {
   return (
-    <>
-    <ShaderBackground />
-    <Row className="appContent">
-      <Col flex="none">
-        <Image
-          alt="basic"
-          src={horse}
-          height="100vh"
-          width="auto"
-          preview={false}
-        />
-      </Col>
-      <Col flex="auto" style={{ width:"10px" }}>
-        <CustomCarousel />
-      </Col>
-    </Row>
-    </>
+    <div className="appContent">
+      <CustomCarousel />
+    </div>
   );
 }
 
